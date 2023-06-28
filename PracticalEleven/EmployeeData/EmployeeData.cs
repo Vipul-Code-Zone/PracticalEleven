e18d6584e108903a9d0d8d8639777e99d1a41831
Vipul1432 <vipul.v@simformsolutions.com>
2023-06-28 20:07:30 +0530

    Feat: add Test 1

---
+++ PracticalEleven/EmployeeData/EmployeeData.cs
@@ -0,0 +1,58 @@
+﻿using PracticalEleven.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Xml.Linq;
+
+namespace PracticalEleven.EmployeeData
+{
+	public static class EmployeeData
+	{
+		private static List<Employee> _employees = new List<Employee>();
+		static EmployeeData()
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				_employees.Add(new Employee()
+				{
+					Id = i,
+					Name = $"Test{i}",
+					DateOfBirth = DateTime.Today.ToString("yyyy-MM-dd"),
+					Address = $"Hello this is address! {i * 568232}"
+				});
+			}
+		}
+        public static List<Employee> GetAllEmployee()
+		{
+			return _employees;
+		}
+		public static Employee GetEmployee(int id)
+		{
+			return _employees.Find(x => x.Id == id);
+		}
+
+		public static void AddEmployee(Employee employee)
+		{
+			employee.Id = _employees.Count;
+			_employees.Add(employee);
+		}
+
+		public static void UpdateEmployee(Employee employee)
+		{
+			var GetEmp = _employees.Find(x => x.Id == employee.Id);
+			if (GetEmp != null)
+			{
+				GetEmp.Name = employee.Name;
+				GetEmp.DateOfBirth = employee.DateOfBirth;
+				GetEmp.Address = employee.Address;
+			}
+		}
+
+		public static void DeleteEmployee(int id)
+		{
+			_employees.Remove(_employees.Find(x => id == x.Id));
+		}
+	}
+}
---
+++ PracticalEleven/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PracticalEleven.Models
+{
+	public class Employee
+	{
+		public int Id { get; set; }
+		[Required(ErrorMessage = "Name is Required Please Fill")]
+		public string Name { get; set; }
+		[DisplayName("Date of Birth")]
+		[Required(ErrorMessage = "Date of Birth is Required Please Fill")]
+		[DataType(DataType.Date), DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+		public string DateOfBirth { get; set; }
+		[Required(ErrorMessage = "Address is Required Please Fill")]
+		public string Address { get; set; }
+	}
+}
